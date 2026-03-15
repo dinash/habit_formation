3e93b341f366a90114db4628a13e6d7cf655dd86
Dinashbabu <dinash.cse@gmail.com>
2026-03-15 18:17:53 +0100

    added a `textColor` parameter to `BoldTextWidget` and updated the `TextStyle` to use it

--- lib/component/bold_text_widget.dart
+++ lib/component/bold_text_widget.dart
@@ -4,14 +4,18 @@ class BoldTextWidget extends StatelessWidget {
   final String text;
   final double fontSize;
   final TextAlign textAlign;
+  final Color textColor;
 
-  const BoldTextWidget({super.key, required this.text, this.fontSize = 25, this.textAlign = TextAlign.center});
+  const BoldTextWidget(
+      {super.key, required this.text, this.fontSize = 25, this.textAlign = TextAlign
+          .center, this.textColor = Colors.black});
 
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
-      style: TextStyle(fontSize: fontSize, fontWeight: FontWeight.bold),
+      style: TextStyle(
+          fontSize: fontSize, fontWeight: FontWeight.bold, color: textColor),
       textAlign: textAlign,
     );
   }
